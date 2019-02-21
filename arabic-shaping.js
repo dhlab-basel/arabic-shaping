5a71d8f75c046f5a60f89ac0ec57c9da07ee23c9
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-02-21 18:14:31 +0100

    feature: Split a string into char groups and add ZWJ where needed.
    - Add tests.

--- arabic-shaping.js
+++ arabic-shaping.js
@@ -1,15 +1,67 @@
-const arabic_shaping_data = require("./arabic-shaping-data.json");
+const arabicShapingData = require("./arabic-shaping-data.json");
 
-exports.isDiacritic = isDiacritic;
+/**
+ * Returns true if the specific character is an Arabic character.
+ *
+ * @param char the character to be tested.
+ * @returns {boolean}
+ */
+function isArabicChar(char) {
+    if (char === undefined || char === null || char === "") {
+        return false;
+    }
 
-exports.addZwj = function(charGroup, previousCharGroup, nextCharGroup) {
+    const codePoint = char.codePointAt(0);
+
+    return (codePoint >= 0x0600 && codePoint <= 0x06FF) ||
+        (codePoint >= 0x0750 && codePoint <= 0x077F) ||
+        (codePoint >= 0x08A0 && codePoint <= 0x08FF) ||
+        (codePoint >= 0xFB50 && codePoint <= 0xFDFF) ||
+        (codePoint >= 0xFE70 && codePoint <= 0xFEFF) ||
+        (codePoint >= 0x10E60 && codePoint <= 0x10E7F) ||
+        (codePoint >= 0x1EC70 && codePoint <= 0x1ECBF) ||
+        (codePoint >= 0x1EE00 && codePoint <= 0x1EEFF)
+}
+
+exports.isArabicChar = isArabicChar;
+
+/**
+ * Returns true if the specified character is an Arabic diacritic.
+ *
+ * @param char the character to be tested.
+ * @returns {boolean}
+ */
+function isArabicDiacritic(char) {
+    if (!isArabicChar(char)) {
+        return false;
+    } else {
+        return arabicDiacritics.has(charToCodePointStr(char));
+    }
+}
+
+exports.isArabicDiacritic = isArabicDiacritic;
+
+/**
+ * Given a character group C (i.e. a letter, possibly followed by one or more
+ * diacritics), as well as the previous character group and the following
+ * one, returns C with any necessary ZWJ added. If charGroup does not contain an
+ * Arabic letter, it is returned unchanged.
+ *
+ * @param charGroup the character group that may need zero-width joining characters.
+ * @param previousCharGroup the previous character group, or null if this is the beginning
+ *        of the text.
+ * @param nextCharGroup the next character group, or null if this is this is the end of the
+ *        text.
+ * @returns charGroup with any necessary zero-width joining characters added.
+ */
+function addZwj(charGroup, previousCharGroup, nextCharGroup) {
     const char = removeDiacritics(charGroup);
     const previousChar = removeDiacritics(previousCharGroup);
     const nextChar = removeDiacritics(nextCharGroup);
 
     const zwjInstruction = getZwjInstruction(char, previousChar, nextChar);
 
-    switch(zwjInstruction) {
+    switch (zwjInstruction) {
         case ZwjInstructions.ZwjBefore:
             return ZWJ + charGroup;
 
@@ -22,11 +74,80 @@ exports.addZwj = function(charGroup, previousCharGroup, nextCharGroup) {
         default:
             return charGroup;
     }
-};
+}
+
+exports.addZwj = addZwj;
+
+/**
+ * Returns the next character group starting at the
+ * specified position in a string. Each Arabic letter and its diacritics
+ * become a group. Any other character becomes a group by itself.
+ *
+ * @param str the string.
+ * @param startPos the position of the first character in
+ *        the character group.
+ * @returns {string}
+ */
+function getNextCharGroup(str, startPos) {
+    let charGroup = "";
+
+    for (let pos = startPos; pos < str.length; pos++) {
+        const char = str.charAt(pos);
+
+        if (charGroup === "") {
+            charGroup = char;
+        } else if (isArabicDiacritic(char)) {
+            charGroup += char;
+        } else {
+            break;
+        }
+    }
+
+    return charGroup;
+}
+
+exports.getNextCharGroup = getNextCharGroup;
+
+/**
+ * Splits a string into an array of character groups and adds ZWJ
+ * to each group as needed. Each Arabic letter and its diacritics
+ * become a group. Any other character becomes a group by itself.
+ *
+ * @param str the string to be transformed.
+ */
+function makeCharGroupsWithZwj(str) {
+    const charGroups = [];
+    let pos = 0;
+
+    while (pos < str.length) {
+        const charGroup = getNextCharGroup(str, pos);
+        charGroups.push(charGroup);
+        pos += charGroup.length;
+    }
+
+    return charGroups.map(
+        function (charGroup, index) {
+            let previousCharGroup = null;
+            let nextCharGroup = null;
+
+            if (index > 0) {
+                previousCharGroup = charGroups[index - 1];
+            }
+
+            if (index < charGroups.length - 1) {
+                nextCharGroup = charGroups[index + 1];
+            }
+
+            return addZwj(charGroup, previousCharGroup, nextCharGroup);
+        }
+    );
+}
+
+exports.makeCharGroupsWithZwj = makeCharGroupsWithZwj;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-ZWJ = "&zwj;";
+const ZWJ = "&zwj;";
 
 function removeDiacritics(str) {
     if (str === undefined || str === null || str === "") {
@@ -38,20 +159,12 @@ function removeDiacritics(str) {
     for (let i = 0; i < str.length; i++) {
         let char = str.charAt(i);
 
-        if (!isDiacritic(char)) {
+        if (!isArabicDiacritic(char)) {
             result += char;
         }
     }
 
     return result;
-}
-
-function isDiacritic(char) {
-    if (!isArabicChar(char)) {
-        return false;
-    } else {
-        return diacritics.has(charToCodePointStr(char));
-    }
 }
 
 const ZwjInstructions = {
@@ -82,23 +195,6 @@ function getZwjInstruction(char, previousChar, nextChar) {
     }
 }
 
-function isArabicChar(char) {
-    if (char === undefined || char === null || char === "") {
-        return false;
-    }
-
-    const codePoint = char.codePointAt(0);
-
-    return (codePoint >= 0x0600 && codePoint <= 0x06FF) ||
-        (codePoint >= 0x0750 && codePoint <= 0x077F) ||
-        (codePoint >= 0x08A0 && codePoint <= 0x08FF) ||
-        (codePoint >= 0xFB50 && codePoint <= 0xFDFF) ||
-        (codePoint >= 0xFE70 && codePoint <= 0xFEFF) ||
-        (codePoint >= 0x10E60 && codePoint <= 0x10E7F) ||
-        (codePoint >= 0x1EC70 && codePoint <= 0x1ECBF) ||
-        (codePoint >= 0x1EE00 && codePoint <= 0x1EEFF)
-}
-
 function charToCodePointStr(char) {
     return char.codePointAt(0).toString(16).toUpperCase();
 }
@@ -120,10 +216,10 @@ function getJoiningProperty(char) {
     } else {
         const codePointStr = charToCodePointStr(char);
 
-        if (diacritics.has(codePointStr)) {
+        if (arabicDiacritics.has(codePointStr)) {
             return JoiningProperties.Transparent;
         } else {
-            const char_shaping_data = arabic_shaping_data[codePointStr];
+            const char_shaping_data = arabicShapingData[codePointStr];
 
             if (char_shaping_data === undefined) {
                 return JoiningProperties.NonJoining;
@@ -143,7 +239,7 @@ function joinsRight(joiningProperty) {
 }
 
 // A set of all the non-spacing characters in the Arabic Unicode character block 0600-06FF.
-const diacritics = new Set([
+const arabicDiacritics = new Set([
     "610",
     "611",
     "612",
