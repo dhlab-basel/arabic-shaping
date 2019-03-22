d7bf2a1aef9657055e9bcc2c2fcbee7dc12bcbd6
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-03-22 09:34:16 +0100

    Change example text to the one from standoff-properties-editor.

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ requirejs.config({
 require(['jquery', 'arabic-shaping'], function ($, arabicShaping) {
     $(document).ready(function () {
         // An example string.
-        const str = "يُلاقي اهْتِماماً واسِعاً";
+        const str = "بدأ العام الدراسى الجديد , و فرح التلاميذُ كثيراً بذهابهم إلى المدرسة و مقابلة زملاءهم . و دخل المُدرسُّ الفصلَ مبتسماً , و كان مسروراً بنظام التلاميذ و حماسهم , و قال لهم : ” اجتهدوا فالعلمُ أساسُ التقدم , و ليس النجاحُ صعباً على المجتهدين , و المجتهدُ دائماً يحافظُ على وقته .” و قد أصبحَ الحاسوب ضرورةً من ضرورات الحياة و مهارةً لازمةً لكل عمل . و ما زالَ الإنسان متعلماً فى مدرسة الحياة حتى يلاقى حتفه .";
 
         // Convert the string to an array of character groups. Each group contains at most one letter and
         // its diacritics, with any necessary ZWJ characters added.
